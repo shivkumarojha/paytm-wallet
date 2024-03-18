06586a36d16acbf9170bc2f836a49a186dd64c4e
shivkumarojha <ojhashivkumar88@gmail.com>
2024-03-18 16:56:14 +0530

    feat(account):implemented transfre route

--- backend/routes/accounts.ts
+++ backend/routes/accounts.ts
@@ -4,14 +4,15 @@ import authenticateUser from "../middleware/auth.middleware";
 import { Request, Response } from "express";
 import { Account } from "../models/account.models";
 import { RequestWithUserId } from "../types/interfaces";
+import { User } from "../models/user.models";
 const router = express.Router()
 
 // Route: for get user balance
 router.get("/balance", authenticateUser, async (req: Request, res: Response) => {
     const userId = (req as RequestWithUserId).userId
-    console.log(userId)
+    
     const account = await Account.find({userId: userId})
-    console.log(account)
+    
     if(!account) {
         return res.status(400).json({
             message: "User have no initial balance"
@@ -22,4 +23,53 @@ router.get("/balance", authenticateUser, async (req: Request, res: Response) => 
     })
 })
 
+// Transfer money to different account
+router.post("/transfer", authenticateUser, async(req: Request, res: Response) => {
+    const {to, amount } = req.body
+    const senderId = (req as RequestWithUserId).userId
+    try {
+
+        const sender = await Account.find({userId: senderId})
+
+        // Check if sender exists
+        if(!sender) {
+            return res.status(200).json({message: "Can't fetch sender from database"})
+        }
+        
+        // check if the sender has sufficient balance
+        if(sender[0].balance < amount) {
+             return res.status(400).json({message: "Insufficient balance!!!"})
+        }
+
+        // find reciever using user id
+        const receiver = await User.findById(to)
+        if(!receiver) {
+            return res.status(400).json({message: "Invalid Account to send money!!"})
+        }
+
+        // Debit amount from the sender
+        await Account.updateOne({
+            userId: senderId
+        }, {
+            $inc: {
+                balance: -amount
+            }
+        })
+        
+        // credit amount to receiver account
+        await Account.updateOne({
+            userId: to
+        }, {
+            $inc: {
+                balance: amount
+            }
+        })
+
+        res.status(200).json({message: "Transfer Successfull"})
+
+    } catch(error) {
+        res.status(401).json({message: error || "Some db error occured"})
+    }
+
+})
 export default router
